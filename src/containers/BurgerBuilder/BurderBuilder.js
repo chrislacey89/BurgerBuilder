6792a4b304715c5002e91743aa5993e490edf62f
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-02 20:34:49 -0500

    set up navigation

--- src/containers/BurgerBuilder/BurderBuilder.js
+++ src/containers/BurgerBuilder/BurderBuilder.js
@@ -115,30 +115,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // alert("You continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Chris Lacey",
-        address: {
-          street: "Test Ave 1",
-          zipcode: 47401,
-          country: "USA"
-        },
-        email: "test@gmail.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        //encodes elements so that they can be used in URL
+        encodeURIComponent(i) +
+          "=" +
+          //encodes the value - which probably wouldnt be needed because it is non critical
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+
+    queryParams.push("price=" + this.state.totalPrice);
+    // join these with an & symbol
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
+    });
   };
 
   render() {
