2d3bda5d4b30ac62e3f12b52dc596179519501be
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-20 06:41:58 -0400

    finished ingredient reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,42 @@
-import * actionTypes from './actions';
+import * as actionTypes from "./actions";
 
 const initialState = {
-    ingredients: null,
-    totalPrice: 4,
-}
-
-const reducer = (state = initialState, action) => {
-//
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  },
+  totalPrice: 4
 };
 
-export default reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          // this clone goes even deeper to access ingredients
+          ...state.ingredients,
+          // [whatever we receive as a payload of the action will receive a new value]
+          // with : we set the new value
+          [action.ingredientName]: state.ingredients[action.ingredientName + 1]
+        }
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          // this clone goes even deeper to access ingredients
+          ...state.ingredients,
+          // [whatever we receive as a payload of the action will receive a new value]
+          // with : we set the new value
+          [action.ingredientName]: state.ingredients[action.ingredientName - 1]
+        }
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
