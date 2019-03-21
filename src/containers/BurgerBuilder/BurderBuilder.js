d19b54131f7ccd8acf476f4a8653a26b6451144b
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-21 06:51:40 -0400

    added price functionality

--- src/containers/BurgerBuilder/BurderBuilder.js
+++ src/containers/BurgerBuilder/BurderBuilder.js
@@ -11,16 +11,8 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import axios from "../../axios-orders";
 import * as actionTypes from "../../store/actions";
 
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.7
-};
-
 class BurgerBuilder extends Component {
   state = {
-    totalPrice: 4,
     purchasable: false,
     purchasing: false,
     loading: false
@@ -48,63 +40,6 @@ class BurgerBuilder extends Component {
       }, 0);
     this.setState({ purchasable: sum > 0 });
   }
-
-  addIngredientHandler = type => {
-    //get count for each number of ingredient
-    const oldCount = this.state.ingredients[type];
-    // every time we click the ADD button we update the old count by 1
-    const updatedCount = oldCount + 1;
-    // clone the state
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    //update cloned state with new count
-    updatedIngredients[type] = updatedCount;
-
-    // get price of ingredient we want to add
-    const priceAddition = INGREDIENT_PRICES[type];
-    // clone price
-    const oldPrice = this.state.totalPrice;
-    // every time we click the ADD button we update the old count by cost of ingredient
-    const newPrice = oldPrice + priceAddition;
-
-    // update state with new ingredient numbers and total price
-    this.setState({
-      totalPrice: newPrice,
-      ingredients: updatedIngredients
-    });
-    this.updatePurchaseState(updatedIngredients);
-  };
-
-  removeIngredientHandler = type => {
-    //get count for each number of ingredient
-    const oldCount = this.state.ingredients[type];
-    if (oldCount <= 0) {
-      return;
-    }
-    // every time we click the ADD button we update the old count by 1
-    const updatedCount = oldCount - 1;
-    // clone the state
-    const updatedIngredients = {
-      ...this.state.ingredients
-    };
-    //update cloned state with new count
-    updatedIngredients[type] = updatedCount;
-
-    // get price of ingredient we want to add
-    const priceDeduction = INGREDIENT_PRICES[type];
-    // clone price
-    const oldPrice = this.state.totalPrice;
-    // every time we click the ADD button we update the old count by cost of ingredient
-    const newPrice = oldPrice - priceDeduction;
-
-    // update state with new ingredient numbers and total price
-    this.setState({
-      totalPrice: newPrice,
-      ingredients: updatedIngredients
-    });
-    this.updatePurchaseState(updatedIngredients);
-  };
 
   purchaseHandler = () => {
     this.setState({ purchasing: true });
@@ -160,7 +95,7 @@ class BurgerBuilder extends Component {
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
-            price={this.state.totalPrice}
+            price={this.props.price}
             ordered={this.purchaseHandler}
           />
         </Aux>
@@ -170,7 +105,7 @@ class BurgerBuilder extends Component {
           ingredients={this.props.ings}
           purchaseCanceled={this.purhaseCancelHandler}
           purchaseContinued={this.purchaseContinueHandler}
-          price={this.state.totalPrice}
+          price={this.props.price}
         />
       );
     }
@@ -195,7 +130,8 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
   return {
     //gets access to ingredients property in state from reducer
-    ings: state.ingredients
+    ings: state.ingredients,
+    price: state.totalPrice
   };
 };
 
