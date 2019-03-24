2effc1fc4bd8620ad1a8da77b6643a7e81256e98
Chris Lacey <ChrisLacey89@gmail.com>
2019-03-24 13:17:46 -0400

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
@@ -20,8 +23,4 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
