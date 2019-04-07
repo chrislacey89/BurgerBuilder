e4088d3c41a37fb7cf4397432414a17998b58131
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-07 16:10:07 -0400

    added actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import Input from "../../components/UI/Input/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.module.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component {
   state = {
@@ -125,10 +128,13 @@ class Auth extends Component {
   }
 }
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     onAuth: (email, password) => dispatch(actions.auth(email, password))
-//   };
-// };
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
 
-export default Auth;
+export default connect(
+  null,
+  mapDispatchToProps
+)(Auth);
