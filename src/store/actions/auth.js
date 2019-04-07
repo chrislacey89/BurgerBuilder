c01665bacd20f236b26a5207a09cabd9747c5aa3
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-07 16:51:00 -0400

    getting a tolken from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -21,8 +23,50 @@ export const authFail = error => {
 };
 
 // a sync code
+
+// export const auth = (email, password) => {
+//   return dispatch => {
+//     dispatch(authStart());
+//     const authData = {
+//       email: email,
+//       password: password,
+//       returnSecureToken: true
+//     };
+//     axios
+//       .post(
+//         "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyCGWszuKAFC_ak3WJpzSrr2QR8biH59Eig",
+//         authData
+//       )
+//       .then(response => {
+//         console.log(response);
+//         dispatch(authSucess(response.data));
+//       })
+//       .catch(err => {
+//         dispatch(authFail(err));
+//       });
+//   };
+// };
+
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCGWszuKAFC_ak3WJpzSrr2QR8biH59Eig",
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSucess(response.data));
+      })
+      .catch(err => {
+        console.log(authData);
+        dispatch(authFail(err));
+      });
   };
 };
