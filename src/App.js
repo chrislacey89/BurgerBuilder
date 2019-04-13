cbe47d54718a8dbff15286766f6dd3c23e65a3c8
Chris Lacey <ChrisLacey89@gmail.com>
2019-04-13 05:49:38 -0400

    fixed burgerbuilder no burder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurderBuilder";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Checkout/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
